0bc2920dd41927872ef6776eee0ada126f1d1cca
mayank <mayank8005@gmail.com>
2021-04-17 02:25:04 +0530

    add sounds in constants

--- src/App.contants.js
+++ src/App.contants.js
@@ -1,6 +1,13 @@
 export const BACKGROUND_IMAGE_URL = `${process.env.PUBLIC_URL}/audience.jpg`;
 
 export const SOUNDS = [
-    {soundName: 'test', soundKey: 'T'},
-    {soundName: 'test2', soundKey: 'S'}
+    {soundName: 'Boom', soundKey: 'A', fileName: 'boom.wav'},
+    {soundName: 'Hihat', soundKey: 'S', fileName: 'hihat.wav'},
+    {soundName: 'Openhat', soundKey: 'D', fileName: 'openhat.wav'},
+    {soundName: 'Snare', soundKey: 'F', fileName: 'snare.wav'},
+    {soundName: 'Tom', soundKey: 'G', fileName: 'tom.wav'},
+    {soundName: 'Clap', soundKey: 'H', fileName: 'clap.wav'},
+    {soundName: 'Kick', soundKey: 'J', fileName: 'kick.wav'},
+    {soundName: 'Ride', soundKey: 'K', fileName: 'ride.wav'},
+    {soundName: 'Tink', soundKey: 'L', fileName: 'tink.wav'},
 ];
